76b2a4d8d9a48db17bc3edac8bfde106afa5528c
Tatiana2504 <77538034+Tatiana2504@users.noreply.github.com>
2021-04-07 23:57:02 +0300

    1/1

---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.metanit;
+
+import java.net.*;
+
+public class Main {
+    public static void main(String[] args) {
+       InetAddress myIP = null;
+       try{
+           myIP = InetAddress.getLocalHost();}
+       catch (UnknownHostException e){}
+       System.out.println(myIP);
+
+    }
+}
