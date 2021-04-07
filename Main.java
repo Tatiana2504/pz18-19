ca40470f975a8caddad06779e0e6df8bd2e5d1b4
Tatiana2504 <77538034+Tatiana2504@users.noreply.github.com>
2021-04-08 00:10:04 +0300

    1/3

--- Main.java
+++ Main.java
@@ -1,14 +1,19 @@
 package com.metanit;
 
 import java.net.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-       InetAddress bsu = null;
-       try{
-           bsu = InetAddress.getByName("www.sutkt.ru");}
-       catch (UnknownHostException e){}
-       System.out.println(bsu);
+    public static void main(String[] args) throws Exception {
+       URL aURL = new URL("http://java.sun.com:80/docs/books/tutorial" + "/index.html?name=networking#DOWNLOADING");
+       System.out.println("protocol = " + aURL.getProtocol());
+        System.out.println("authority = " + aURL.getAuthority());
+        System.out.println("host = " + aURL.getHost());
+        System.out.println("port = " + aURL.getPort());
+        System.out.println("path = " + aURL.getPath());
+        System.out.println("query = " + aURL.getQuery());
+        System.out.println("filename = " + aURL.getFile());
+        System.out.println("ref = " + aURL.getRef());
 
     }
 }
